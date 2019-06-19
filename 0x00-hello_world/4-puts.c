5ff99dada19813f25ca7998f83063039d1f869a9
David Peralta <924@holbertonschool.com>
2019-06-19 12:37:27 -0500

    Fixing code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+/*This is a puts function
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
