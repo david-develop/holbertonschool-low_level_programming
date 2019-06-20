26e4396a999a40ce893112954804a0d54ac097d8
David Peralta <924@holbertonschool.com>
2019-06-20 17:06:51 -0500

    iterations with conditional

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			for (z = y +1; z <= '9'; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if (!(y == '8' && x == '7' && z == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
