4de6caefdad0ebcf9e30e6132d1502c2b5c2a787
David Peralta <924@holbertonschool.com>
2019-06-20 13:36:50 -0500

    program shows all alphabet but q and e

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,10 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch == 'e')
-			continue;
-		if (ch == 'q')
-			continue;
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
 	}
 	putchar('\n');
 	return (0);
