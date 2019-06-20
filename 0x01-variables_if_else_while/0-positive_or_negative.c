f96622ab447622016552be5977811999034543bb
David Peralta <924@holbertonschool.com>
2019-06-20 10:25:54 -0500

    ramdom number program

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n>0)
+	{
+		printf("%d", n);
+		printf("%s\n", "is positive");
+	}
+	else if (n==0)
+	{
+		printf("%d", n);
+		printf("%s\n", "is zero");
+	}
+	else
+	{
+		printf("%d", n);
+		printf("%s\n", "is negative");
+	}
+	return (0);
+}
