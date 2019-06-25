df829561dd9b4c392711a9fc9cffd18a7ff38811
David Peralta <924@holbertonschool.com>
2019-06-25 09:24:10 -0500

    commenting variables

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  * _islower - Verify if the letter is lowercase.
- *
+ * @c: variable for input value.
+ * Return: when is a lower letter 1.
  */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
