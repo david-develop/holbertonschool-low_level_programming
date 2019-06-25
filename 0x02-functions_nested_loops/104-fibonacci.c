f3b0ff54ddec6017805641f0de04e78cef2d8ab4
David Peralta <924@holbertonschool.com>
2019-06-25 18:10:47 -0500

    until 98 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,29 +8,44 @@
 
 int main(void)
 {
-	unsigned long int in, ln, iter, itnum;
-        unsigned long int next;
+	long int in, ln, iter, itnum, next, ini, lni, inl, lnl, nexti, nextl;
+	long int itnuml = 6, tendig = 1000000000;
 
 	in = 1;
 	ln = 2;
-	itnum = 95;
-	printf("%ld,\n", in);
-	printf("%ld,\n", ln);
+	itnum = 88;
+	printf("%ld, ", in);
+	printf("%ld, ", ln);
 
 	for (iter = 0; iter <= itnum; iter++)
-	{
-		next = in + ln;
-		printf("%ld", next);
-		if (!(iter == itnum))
-		{
-			printf("%s", ",\n");
-		}
-		else
-		{
-			printf("%s", "\n");
-		}
+	{ next = in + ln;
+		printf("%ld, ", next);
 		in = ln;
 		ln = next;
+	}
+	for (iter = 0; iter <= itnuml; iter++)
+	{
+		if (iter == 0)
+		{ ini = in % tendig;
+			lni = ln % tendig;
+			inl = in / tendig;
+			lnl = ln / tendig;
+		}
+		else
+		{ ini = lni;
+			inl = lnl;
+			lni = nexti;
+			lnl = nextl;
+		}
+		nexti = (ini + lni) % tendig;
+		nextl = inl + lnl + (nexti / tendig);
+		printf("%ld%ld", nextl, nexti);
+		if (!(iter == itnuml))
+		{ printf("%s", ", ");
+		}
+		else
+		{ printf("%s", "\n");
+		}
 	}
 	return (0);
 }
