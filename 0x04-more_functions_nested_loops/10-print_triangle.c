fa55f20f0fbf807ef17c3c6a803c088259eb8caf
David Peralta <924@holbertonschool.com>
2019-06-27 14:28:40 -0500

    print triangle reverse

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_triangle - Prints a triangle from the size value.
+ * @size: is the size of the triangle, defined the by input.
+ */
+
+void print_triangle(int size)
+{
+	char a = 35, x, y, z, w;
+
+	if (size > 0)
+	{
+		for (y = size; y > 0; y--)
+		{
+			for (x = 0; x < y; x++)
+			{
+				_putchar(' ');
+			}
+			w = size - x;
+			for (z = 0; z <= w; z++)
+			{
+				_putchar(a);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
