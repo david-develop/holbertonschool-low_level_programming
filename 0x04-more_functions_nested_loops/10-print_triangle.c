8f9d06543b68cc597b61ae3a551a251eca11e255
David Peralta <924@holbertonschool.com>
2019-06-27 14:36:46 -0500

    print triangle reverse

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,12 +12,12 @@ void print_triangle(int size)
 	{
 		for (y = size; y > 0; y--)
 		{
-			for (x = 0; x < y; x++)
+			for (x = 0; x < (y - 1); x++)
 			{
 				_putchar(' ');
 			}
 			w = size - x;
-			for (z = 0; z <= w; z++)
+			for (z = 0; z <= (w - 1); z++)
 			{
 				_putchar(a);
 			}
