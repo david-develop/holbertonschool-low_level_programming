d00f98daa5ebc013c24300bc5b306d258f05bfa0
David Peralta <924@holbertonschool.com>
2019-07-02 10:16:13 -0500

    reverse function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,19 @@
 void rev_string(char *s)
 {
 	int i = 0, iter;
-	char ini_d, last_d, c;
+	char last_d, c;
 
 	/*count the lenght of the string*/
 	while (*(s + i) != '\0')
 	{
 		i++;
 	}
-	for (iter = 0; iter < (i / 2); iter++)
+	for (iter = 0; iter < i; iter++)
 	{
 		c = s[iter];
 		last_d = s[i - 1];
-		ini_d = c;
 		s[iter] = last_d;
-		s[i] = ini_d;
+		s[i - 1] = c;
 		i--;
 	}
 }
