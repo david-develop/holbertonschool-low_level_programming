0113e420346b8a35d9592655dbd0d84456f9bff9
David Peralta <924@holbertonschool.com>
2019-07-04 17:00:24 -0500

    enconding to leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * leet - encode the string to leet.
+ * @s: given string.
+ * Return: the string encoded.
+ */
+char *leet(char *s)
+{
+	int iter, i;
+	char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char n[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (iter = 0; s[iter] != '\0'; iter++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (s[iter] == a[i])
+				s[iter] = n[i];
+			else
+				continue;
+		}
+	}
+	return (s);
+}
