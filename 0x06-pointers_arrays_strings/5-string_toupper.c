27678f6e5aeabe053cb6e83d2da3c8cf8577d15d
David Peralta <924@holbertonschool.com>
2019-07-04 15:19:29 -0500

    change to upper case when lower

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * string_toupper - print the given string in reverse.
+ *
+ * Return: the string in upper.
+ */
+char *string_toupper(char *s)
+{
+	int iter;
+	for (iter = 0; s[iter] != '\0'; iter++)
+	{
+		if (s[iter] >= 'a' && s[iter] <= 'z')
+		{
+			s[iter] += 'A' - 'a';
+		}
+		else
+			continue;
+	}
+	return (s);
+}
