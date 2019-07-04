72e7d9a2de0feaff8e10299100adfb1330aeff02
David Peralta <924@holbertonschool.com>
2019-07-04 17:24:11 -0500

    print integrer

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	int dig = 0, tend = 1, iter = 1;
+	unsigned int abs = n, numch, num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		abs = -n;
+	}
+	else
+	{
+		abs = n;
+	}
+	num = abs;
+
+	while (num > 0)
+	{
+		num /= 10;
+		dig++;
+	}
+	while (iter < dig)
+	{
+		tend *= 10;
+		iter++;
+	}
+	while (tend >= 1)
+	{
+		numch = (abs / tend) % 10;
+		_putchar(numch + '0');
+		tend /= 10;
+	}
+}
