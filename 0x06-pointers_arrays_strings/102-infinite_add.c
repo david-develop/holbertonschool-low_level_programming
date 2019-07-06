5d4ff04610e56c42866ca6591b1a0a41365141a9
David Peralta <924@holbertonschool.com>
2019-07-05 22:54:29 -0500

    finxing the adding operations

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -10,40 +10,43 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int leng_1, leng_2, higher, carry = 0, add;
+	int leng_1 = 0, leng_2 = 0, higher, carry = 0, add, add_1, add_2;
 
-	/*get the values lenght*/
-	for (leng_1 = 0; n1[leng_1] != '\0'; leng_1++);
-
-	for (leng_2 = 0; n2[leng_2] != '\0'; leng_2++);
-
-	/*printf("leng_1: %d, leng_2: %d\n", leng_1, leng_2);*/
-	/*determine which is the bigger number and store it in a variable*/
+	for (leng_1 = 0; n1[leng_1] != '\0'; leng_1++)
+		;
+	for (leng_2 = 0; n2[leng_2] != '\0'; leng_2++)
+		;
 	if (leng_1 > leng_2)
 		higher = leng_1;
 	else
 		higher = leng_2;
-
-	/*if the size of the resultant is bigger than the buffer size_r*/
-	/*return 0, else add a null point in the end of the resultant string*/
 	if (size_r <= (higher + 1))
 		return (0);
-	else
-		r[higher + 1] = '\n';
-
+	r[higher + 1] = '\0';
+	leng_1--, leng_2--, add_1 = n1[leng_1] - '0', add_2 = n2[leng_2] - '0';
 	while (higher >= 0)
 	{
-		if (leng_1 >= 0 && leng_2 >= 0)
-			add = ((*(n1 + leng_1) - '0') + (*(n2 + leng_2) - '0') + carry);
-		else if (leng_1 < 0 && leng_2 >= 0)
-			add = ((*(n2 + leng_2) - '0') + carry);
-		else if (leng_1 > 0 && leng_2 < 0)
-			add = ((*(n1 + leng_1) - '0') + carry);
-		*(r + higher) = (add % 10) + '0';
-		carry = add / 10;
-		leng_1--;
-		leng_2--;
+		add = add_1 + add_2 + carry;
+		if (add >= 10)
+			carry = add / 10;
+		else
+			carry = 0;
+		if (add > 0)
+			r[higher] = (add % 10) + '0';
+		else
+			r[higher] = '0';
+		if (leng_1 > 0)
+			leng_1--, add_1 = n1[leng_1] - '0';
+		else
+			add_1 = 0;
+		if (leng_2 > 0)
+			leng_2--, add_2 = n2[leng_2] - '0';
+		else
+			add_2 = 0;
 		higher--;
 	}
-	return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
