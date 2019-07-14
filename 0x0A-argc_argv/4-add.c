765aee3d26e880f9d916609494fd4f2cd4243245
David Peralta <924@holbertonschool.com>
2019-07-13 19:58:30 -0500

    [fix] 4-add.c making the code more efficient

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - program thar print the multiplication result of two numbers.
- * @c: input character to _isdigit function.
+ * @argc: argument count.
+ * @argv: argument vector.
  * Return: 1 if program doesn't recive two arguments, otherwise 0.
  */
-
-int _isdigit(int c);
 
 int main(int argc, char *argv[])
 {
@@ -17,31 +17,18 @@ int main(int argc, char *argv[])
 	{
 		for (array_i = 0; argv[i][array_i] != '\0'; array_i++)
 		{
-			if (!_isdigit(argv[i][array_i]))
+			if (!isdigit(argv[i][array_i]))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-	}
-	for (in = 0; in < argc; in++)
 		sum += atoi(argv[in]);
+	}
+	/**
+	  * for (in = 0; in < argc; in++)
+	  * sum += atoi(argv[in]);
+	  */
 	printf("%d\n", sum);
 	return (0);
-}
-/**
- * _isdigit - function that checks if arguments are digits.
- * @c: input character to _isdigit function.
- * Return: 1 if none a digit and 0 if is digit.
- */
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
 }
