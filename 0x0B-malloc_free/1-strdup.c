690b03891215cc91eed2e874fff670b84f201917
David Peralta <924@holbertonschool.com>
2019-07-16 12:27:41 -0500

    [add] files of project

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - create new space in memory that contains cpy of a given string
+ * @str: given string.
+ * Return: Pointer to allocated memory.
+ */
+char *_strdup(char *str)
+{
+	char *cpy_ar;
+	unsigned int i, cp;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	cpy_ar = malloc(i * sizeof(char));
+
+	if (cpy_ar == NULL)
+		return (NULL);
+
+	for (cp = 0; cp <= i; cp++)
+	{
+		cpy_ar[cp] = str[cp];
+	}
+
+	return (cpy_ar);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings in a new allocated space.
+ * @s1: given string 1.
+ * @s2: given string 2, which is concatenated.
+ * Return: Pointer to allocated memory.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *cat_ar;
+	unsigned int i, j, cp, r, final_leng;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	final_leng = i + j;
+
+	cat_ar = malloc(final_leng * sizeof(char));
+
+	if (cat_ar == NULL)
+		return (NULL);
+
+	for (cp = 0; cp < i; cp++)
+	{
+		cat_ar[cp] = s1[cp];
+	}
+	for (r = 0; r <= j; cp++, r++)
+	{
+		cat_ar[cp] = s2[r];
+	}
+	cat_ar[final_leng] = '\0';
+
+	return (cat_ar);
+}
