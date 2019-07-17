e74278e47e18f8214705502cc26a76cb4857afd5
David Peralta <924@holbertonschool.com>
2019-07-16 20:36:54 -0500

    [add] 100-strtow.c

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * strtow - splits a string into words.
+ * @str: input string.
+ * Return: Pointer to array.
+ */
+char **strtow(char *str)
+{
+	char **p = NULL;
+	int count_w, count_c;
+	int i, leng, x, fr, j;
+
+	count_w = 0;
+	count_c = 0;
+	x = 0;
+	j = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	for (leng = 0; str[leng] != '\0'; leng++)
+		;
+	for (i = 0; i < leng; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for (i++; str[i] != ' '; i++)
+				;
+			count_w++;
+		}
+	}
+	p = malloc(count_w * sizeof(char *));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < leng; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for (count_c = 0; str[i] != ' '; i++)
+				count_c++;
+			p[x] = malloc((count_c + 1) * sizeof(char));
+			if (p[x] == NULL)
+			{
+				for (fr = x; fr > 0; fr--)
+				{
+					free(p[fr]);
+				}
+				return (NULL);
+			}
+			for (j = 0; j < count_c; j++)
+			{
+				p[x][j] = str[(i - count_c) + j];
+			}
+			x++;
+		}
+	}
+	return (p);
+}
