c5ffb43c8a7fa0354ce95ebd23eec6f54aa20e5e
David Peralta <924@holbertonschool.com>
2019-07-18 18:52:31 -0500

    [fix] 101_mul.c - exit option

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
  * main - program thar print the multiplication result of two numbers.
  * @argc: argument count.
@@ -10,7 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul, i, array_i;
+	int mul, i, array_i, len1, len2;
+	char *result = NULL;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -19,6 +21,11 @@ int main(int argc, char *argv[])
 	}
 	else if (argc > 1)
 	{
+		len1 = strlen(argv[1]);
+		len2 = strlen(argv[2]);
+		result = malloc(sizeof(char) * (len1 + len2 + 1));
+		if (result == NULL)
+			return(1);
 		for (i = 1; i < argc; i++)
 		{
 			for (array_i = 0; argv[i][array_i] != '\0'; array_i++)
@@ -33,5 +40,6 @@ int main(int argc, char *argv[])
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 	}
+	free(result);
 	return (0);
 }
