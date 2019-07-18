75c2ff022e7e0098e96b0cd67069858c122cc732
David Peralta <924@holbertonschool.com>
2019-07-18 14:55:13 -0500

    [fix] 3-array_range.c making code simplier

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,20 +9,17 @@
 int *array_range(int min, int max)
 {
 	int *arr_i = NULL;
-	unsigned int i, rang_arr;
+	int i, rang_arr;
 
 	if (min > max)
 		return (NULL);
 
-	if (min == 0)
-		rang_arr = (unsigned int)max - (unsigned int)min + 1;
-	else
-		rang_arr = (unsigned int)max - (unsigned int)min;
+	rang_arr = max - min + 1;
 
 	arr_i = malloc(sizeof(int) * (rang_arr));
 	if (arr_i == NULL)
 		return (NULL);
-	for (i = 0; i < rang_arr && min < max; i++, min++)
+	for (i = 0; min < max; i++, min++)
 	{
 		arr_i[i] = min;
 	}
