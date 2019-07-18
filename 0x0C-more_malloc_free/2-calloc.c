a962aacb412fc07cb19b509240947886b2bf4a27
David Peralta <924@holbertonschool.com>
2019-07-18 13:18:35 -0500

    [fix] 2-calloc.c - returning type

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	mem_as = malloc(nmemb * size);
 	if (mem_as == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		mem_as[i] = 0;
 	}
