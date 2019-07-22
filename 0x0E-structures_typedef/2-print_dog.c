1947b12306fc549dcb7f1211aedb295932c2905d
David Peralta <924@holbertonschool.com>
2019-07-22 14:55:15 -0500

    [fix] 2-print_dog.c conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,5 +8,17 @@
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
 }
