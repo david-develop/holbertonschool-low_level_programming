df97cda994504fea6d977dd176e908b78134bbff
David Peralta <924@holbertonschool.com>
2019-07-22 15:53:51 -0500

    [fix] 4-new_dog.c - requirements

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,17 +2,20 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * print_dog - prints a struct.
- * @d: pointer to struct dog.
+ * new_dog - creates a new struct dog.
+ * @name: input name.
+ * @age: input age.
+ * @owner: input owner name.
+ * Return: pointer to struct.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *nd;
 	int i, j;
 
-	for(i = 0; name[i] != '\0'; i++)
+	for (i = 0; name[i] != '\0'; i++)
 		;
-	for(j = 0; owner[j] != '\0'; j++)
+	for (j = 0; owner[j] != '\0'; j++)
 		;
 
 	nd = malloc(sizeof(dog_t));
