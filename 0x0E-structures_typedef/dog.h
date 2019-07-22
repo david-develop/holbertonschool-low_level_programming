2ae7bf345e41347db86a4b3516177125a159a852
David Peralta <924@holbertonschool.com>
2019-07-22 13:51:23 -0500

    [fix] dog.h adding typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: the name of the owner
  * Description: containt informaction of the dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
