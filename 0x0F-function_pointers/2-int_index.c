632e88975df6b42487d4acf8847811950a1d64ab
David Peralta <924@holbertonschool.com>
2019-07-24 13:56:00 -0500

    [fix] 2-int_index.c - condition fixing

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-		exit(98);
-
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
-	return(-1);
+	return (-1);
 }
