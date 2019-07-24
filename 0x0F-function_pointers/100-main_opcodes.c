316a74d638b3256f29d81ed4a0f93db03aa1460f
David Peralta <924@holbertonschool.com>
2019-07-24 17:19:47 -0500

    [add] 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the opcodes of its own main function.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: pointer to the function that corresponds to the operator given.
+ */
+
+int main(int argc, char *argv[])
+{
+	int b;
+
+	b = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return(0);
+}
