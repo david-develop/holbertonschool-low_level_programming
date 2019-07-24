e9189fbd880c850687a91acfa533fd443721e1eb
David Peralta <924@holbertonschool.com>
2019-07-24 13:16:10 -0500

    [fix] 0-print_name.c - if null condition

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - function that prints a name.
  * @name: string of input name.
@@ -7,5 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		exit(98);
 	f(name);
 }
