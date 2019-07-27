29c0e666cc8345ec78f4d63c213f6a15bce18e42
David Peralta <924@holbertonschool.com>
2019-07-27 13:42:16 -0500

    add 3-print_all.c file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,79 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything.
+ * @format: formats numbers given.
+ */
+
+void print_all(const char * const format, ...)
+{
+	prfor_t form_get[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int i, j, cont;
+	va_list argu;
+
+	va_start(argu, format);
+
+	i = 0;
+	cont = 0;
+
+	while (format[i] && format != NULL)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (form_get[j].form[0] == format[i])
+			{
+				if (cont != 0)
+					printf(", ");
+				(form_get[j].f(argu));
+				cont++;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(argu);
+}
+
+void print_char(va_list va)
+{
+	int c;
+
+	c = va_arg(va, int);
+	printf("%c", c);
+}
+
+void print_integer(va_list va)
+{
+	int i;
+
+	i = va_arg(va, int);
+	printf("%d", i);
+}
+
+void print_float(va_list va)
+{
+	double f;
+
+	f = va_arg(va, double);
+	printf("%f", f);
+}
+
+void print_string(va_list va)
+{
+	char *s;
+
+	s = va_arg(va, char *);
+	printf("%s", s);
+}
