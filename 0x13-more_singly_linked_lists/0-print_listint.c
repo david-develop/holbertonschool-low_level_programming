f197c9a6429fb4cdeec70d12f3396c8c2677b059
David Peralta <924@holbertonschool.com>
2019-08-05 09:56:36 -0500

    adding first files of the project

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: address of head node.
+ * Return: number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	int elements = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+### Singly linked lists
+
+This repository is about singly linked lists
+
+General learning purpose:
+
+- How to use linked lists
+- Start to look for the right source of information without too much help
