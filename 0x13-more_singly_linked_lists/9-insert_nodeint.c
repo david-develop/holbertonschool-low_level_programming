5d832a7e390e4d1313243c03031097979a4339c9
David Peralta <924@holbertonschool.com>
2019-08-06 09:47:19 -0500

    fix 9-insert_nodeint.c add condition for edge case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,10 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-	if (*head == NULL)
-		return (NULL);
 	cursor = *head;
-	if (idx == 0)
+	if (idx == 0 || (idx == 0 && *head == NULL))
 	{
 		new = malloc(sizeof(listint_t));
 		if (new == NULL)
