1bcd86705b5de39bd93bad9f77597e8c80f15417
David Peralta <924@holbertonschool.com>
2019-08-08 13:05:31 -0500

    fix 1-print_binary.c other machines bytes value

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,21 +9,24 @@ void print_binary(unsigned long int n)
 	int flag = 0;
 	int a = 1;
 	int b, i;
+	int size;
 	unsigned long int p;
 
-	for (i = 0; i < 64; i++)
+	if (n == 0)
 	{
-		p = ((a << (63 - i)) & n);
-		if (p >> (63 - i))
+		_putchar('0');
+		return;
+	}
+	size = sizeof(unsigned long int) * 8;
+	for (i = 0; i < size; i++)
+	{
+		p = ((a << ((size - 1) - i)) & n);
+		if (p >> ((size - 1) - i))
 			flag = 1;
 		if (flag)
 		{
-			b = p >> (63 - i);
+			b = p >> ((size - 1) - i);
 			_putchar(b + 48);
 		}
-	}
-	if (!flag)
-	{
-		_putchar('0');
 	}
 }
