69446157af6759a7492ba4a1e7caa82595559a74
David Peralta <924@holbertonschool.com>
2019-08-13 13:24:09 -0500

    fix 1-create_file.c conditions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,9 +27,7 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int i;
 	int sz;
-	char *buf;
 
 	if (filename == NULL)
 		return (-1);
@@ -39,25 +37,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
+		text_content = "";
 
-	buf = malloc(sizeof(char) * (_strlen(text_content) + 1));
-	if (buf == NULL)
-		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		buf[i] = text_content[i];
-	}
-	buf[i] = '\0';
-
-	sz = write(fd, buf, (_strlen(text_content) + 1));
+	sz = write(fd, text_content, _strlen(text_content));
 	if (sz == -1)
 		return (-1);
 
 	close(fd);
-	free(buf);
 	return (1);
 }
