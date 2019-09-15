48efa4c2aa257c5bb7ace1e4ff3c95433c8fb6c6
David Peralta <924@holbertonschool.com>
2019-09-15 14:42:06 -0500

    fix 7instert function prototype

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,30 +1,31 @@
 #include "lists.h"
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
- * @head: pointer to first node.
+ * @h: pointer to first node.
  * @idx: given node index.
  * @n: data to store in the new node.
  * Return: return the address of the new node, or NULL if it failed or
  * if it is not possible to add the new node at index idx.
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *cursor;
 	unsigned int i;
 
-	if (head == NULL)
+	if (h == NULL)
 		return (NULL);
-	cursor = *head;
-	if (idx == 0 || (idx == 0 && *head == NULL))
+	cursor = *h;
+	if (idx == 0 || (idx == 0 && *h == NULL))
 	{
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
-		new->next = *head;
+		new->next = *h;
 		new->prev = NULL;
-		*head = new;
+		*h = new;
 		return (new);
 	}
 	for (i = 0; cursor != NULL; i++, cursor = cursor->next)
