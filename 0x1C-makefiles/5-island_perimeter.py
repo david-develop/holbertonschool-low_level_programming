0889942112bdb7c7e62ec2dc096aef288e1a0478
david-develop <924@holbertonschool.com>
2019-11-15 16:15:44 -0500

    [Add] 5 island perimeter file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """that returns the perimeter of the island described in grid"""
+    count = 0
+    for column in range(len(grid)):
+        for row in range(len(grid[column])):
+            if grid[column][row] == 1:
+                # verify up
+                if column - 1 < 0:
+                    count += 1
+                else:
+                    try:
+                        if grid[column - 1][row] == 0 or column - 1 < 0:
+                            count += 1
+                    except:
+                        pass
+                # verify down
+                if column + 1 > len(grid) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[column + 1][row] == 0:
+                            count += 1
+                    except:
+                        pass
+                # verify right
+                if row + 1 > len(grid[column]) - 1:
+                    count += 1
+                else:
+                    try:
+                        if grid[column][row + 1] == 0:
+                            count += 1
+                    except:
+                        pass
+                # verify left
+                try:
+                    if grid[column][row - 1] == 0 or row - 1 < 0:
+                        count += 1
+                except:
+                    pass
+    return count
